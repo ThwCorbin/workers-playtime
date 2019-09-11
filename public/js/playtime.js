27590edd3a8e98206850f6dcc60aaeaea6fe64c4
NiceDrop <30200976+NiceDrop@users.noreply.github.com>
2019-09-10 23:48:09 -0400

    Configured ESLint Prettier, set up basic assets for LiveShare

--- public/js/playtime.js
+++ public/js/playtime.js
@@ -1,5 +1,27 @@
 let x = 0;
 
-if(x <= 3) {
-  // code
+while (x <= 5) {
+  console.log(x);
+  x++;
 }
+
+let bigFunc = (dog, cat) => {
+  console.log(`${dog} is a dog`);
+  console.log(`${cat} is a cat`);
+};
+
+let smallFunc = (cat, dog = "Joe") => {
+  console.log(`${cat} is a coolio cat`);
+  console.log(`${dog} is no foolio`);
+};
+
+bigFunc("Nellie", "Freddie");
+smallFunc("Freddie");
+
+//# Better Comments Key
+//# Label: (which I custom added to settings.json)
+//! Warning: My cat and dog are dangerous sleepers
+//? Question: for ya'll
+//* Highlight: This is worth mentioning
+////Stike: this code from the Akashic Records
+//todo: unite for playtime
